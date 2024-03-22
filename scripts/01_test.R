90774e147a9f1c327c901117e1409ab1aaab4f6e
Silvan Hess <silvan.hess@stud.unilu.ch>
2024-03-22 15:37:22 +0100

    this is some scripts from todays course fridays 22.3.24

---
+++ scripts/01_test.R
@@ -0,0 +1,16 @@
+#get the urls of the 20 minutes homepage
+url <- "https://www.20min.ch/"
+read_html(url) %>%
+  html_elements(css = "a") %>%
+  html_attr("href")
+
+#load data
+load("data_original/s3-subset.Rdata")
+
+#inspect
+view(dat)
+str(dat)
+
+#extract column ten
+dat[[, 10]]
+
